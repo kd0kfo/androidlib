69c15298c5b0245e6618499db7fc62ab387ba040
David Coss <david@davecoss.com>
2014-02-18 13:40:31 -0600

    Removed generated file.

--- gen/com/davecoss/android/lib/R.java
+++ gen/com/davecoss/android/lib/R.java
@@ -25,16 +25,19 @@ public final class R {
     }
     public static final class id {
         public static int LinearLayout1=0x7f080000;
-        public static int action_settings=0x7f080006;
-        public static int btnSelect=0x7f080005;
-        public static int textView1=0x7f080001;
-        public static int textView2=0x7f080003;
-        public static int txtDirName=0x7f080002;
-        public static int txtFileName=0x7f080004;
+        public static int action_settings=0x7f080008;
+        public static int btnCancel=0x7f080003;
+        public static int btnConfirm=0x7f080002;
+        public static int btnSelect=0x7f080007;
+        public static int textView2=0x7f080005;
+        public static int txtDirName=0x7f080004;
+        public static int txtFileName=0x7f080006;
+        public static int txtMessage=0x7f080001;
     }
     public static final class layout {
-        public static int activity_file_chooser=0x7f030000;
-        public static int activity_save_file_chooser=0x7f030001;
+        public static int activity_confirmation=0x7f030000;
+        public static int activity_file_chooser=0x7f030001;
+        public static int activity_save_file_chooser=0x7f030002;
     }
     public static final class menu {
         public static int file_chooser=0x7f070000;
@@ -43,12 +46,15 @@ public final class R {
     public static final class string {
         public static int action_settings=0x7f050002;
         public static int app_name=0x7f050000;
+        public static int btnCancel=0x7f05000c;
+        public static int btnConfirm=0x7f05000b;
         public static int click_to_select=0x7f050007;
         public static int file=0x7f050008;
         public static int folder=0x7f050006;
         public static int hello_world=0x7f050003;
         public static int select=0x7f050009;
         public static int select_file=0x7f050004;
+        public static int title_activity_confirmation=0x7f05000a;
         public static int title_activity_file_chooser=0x7f050001;
         public static int title_activity_save_file_chooser=0x7f050005;
     }
